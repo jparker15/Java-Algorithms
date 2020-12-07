bea36b8a4acb0e7605c0718a0ab6f621610886d8
Jeremy <jeremyeparker@yahoo.com>
2020-12-06 23:47:03 -0500

    add reverse string method

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -38,11 +38,15 @@ public class Main {
 
         return true;
     }
+
+    public static String reverseA(String msg){
+        return msg;
+    }
 }
 
 
 
-}
+
 /* hello world || ehwni ellawo eiwnd iwr
     h: 1, e:1, l:3, o:2, w:1, r:1, d:1
     e: 3, h: 1, w: 4, n:2, i:3, l:2, a:1, o:1, d:1, r:1
