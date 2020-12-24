838b4178cb42d6c9b38cf072d245229c7bef6e01
Jeremy <jeremyeparker@yahoo.com>
2020-12-23 21:52:08 -0500

    2nd day

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -6,6 +6,17 @@ import java.util.*;
 
 
 public class Main {
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+
+        // Get the Tax
+        double tax = (meal_cost * tax_percent) / 100;
+        // Get the Tip
+        double tip = (meal_cost * tip_percent/100);
+        // Total Cost
+        double totalCost = meal_cost + tax + tip;
+        // Round the total cost and output to console
+        System.out.println(Math.round(totalCost));
+    }
     public static void main(String[] args) {
         // Create a Scanner object to read input from stdin.
         Scanner scan = new Scanner(System.in);
