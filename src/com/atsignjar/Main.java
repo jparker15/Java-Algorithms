262f487440c38d3588ca117c551fd1cb4068572f
Jeremy <jeremyeparker@yahoo.com>
2020-12-28 22:21:22 -0500

    update TODO new algor

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -1,38 +1,15 @@
 package com.atsignjar;
 
-import org.w3c.dom.ls.LSOutput;
-
-import java.util.*;
-
+//TODO:Given an integer,perform the following conditional actions:If  is odd, print Weird
+// If is even and in the inclusive range of to print Not Weird
+// If is even and in the inclusive range of toprint Weird
+// If is even and greater than print Not Weird
+// Complete the stub code provided in your editor to print whether or not is weird.
 
 public class Main {
-    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    int main () {
 
-        // Get the Tax
-        double tax = (meal_cost * tax_percent) / 100;
-        // Get the Tip
-        double tip = (meal_cost * tip_percent/100);
-        // Total Cost
-        double totalCost = meal_cost + tax + tip;
-        // Round the total cost and output to console
-        System.out.println(Math.round(totalCost));
-    }
-    public static void main(String[] args) {
-        // Create a Scanner object to read input from stdin.
-        Scanner scan = new Scanner(System.in);
-
-        // Read a full line of input from stdin and save it to our variable, inputString.
-        String inputString = scan.nextLine();
-
-        // Close the scanner object, because we've finished reading
-        // all of the input from stdin needed for this challenge.
-        scan.close();
-
-        // Print a string literal saying "Hello, World." to stdout.
-        System.out.println("Hello, World.");
-
-        // TODO: Write a line of code here that prints the contents of inputString to stdout.
-        System.out.print(inputString);
+        return 0;
     }
 }
 
