962333ff038542556172a50a48b26fd23e150e38
Jeremy Parker <jeremyeparker@yahoo.com>
2021-02-24 14:52:13 -0500

    intro to condional statements

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -1,23 +1,12 @@
 package com.atsignjar;
 
-//TODO:Given an integer,perform the following conditional actions:If  is odd, print Weird
-// If is even and in the inclusive range of to print Not Weird
-// If is even and in the inclusive range of toprint Weird
-// If is even and greater than print Not Weird
-// Complete the stub code provided in your editor to print whether or not is weird.
 
 public class Main {
-    int main () {
-
-        return 0;
+    public static void main(String[] args) {
+        IntroToConditionalStatements.Conditionals();
     }
 }
 
 
 
-
-/* hello world || ehwni ellawo eiwnd iwr
-    h: 1, e:1, l:3, o:2, w:1, r:1, d:1
-    e: 3, h: 1, w: 4, n:2, i:3, l:2, a:1, o:1, d:1, r:1
-*/
 
