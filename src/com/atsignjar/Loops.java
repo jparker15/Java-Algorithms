fd990457fc6407d0243576e84738174555a667ea
Jeremy Parker <jeremyeparker@yahoo.com>
2021-02-25 19:43:42 -0500

    Given an integer print its first multiples

---
+++ src/com/atsignjar/Loops.java
@@ -0,0 +1,18 @@
+package com.atsignjar;
+
+import java.util.Scanner;
+
+public class Loops {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void Multiples(){
+        System.out.println("Enter a number to get multiples up to 10:");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++){
+            int result = i * n;
+            System.out.println(n + " x " + i + " = " + result);
+        }
+
+    }
+}
