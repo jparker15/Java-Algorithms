37b93a596e3b127049cde722eb4fb7e4b6ef8515
Jeremy Parker <jeremyeparker@yahoo.com>
2021-02-26 19:47:12 -0500

    String Review

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -1,14 +1,22 @@
 package com.atsignjar;
 
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 //      IntroToConditionalStatements.Conditionals();
 
         /*ClassVsInstance person = new ClassVsInstance(24);
         person.amIOld();*/
 
-        Loops.Multiples();
+//      Loops.Multiples();
+
+        StringReview.Review();
+
     }
 }
 
