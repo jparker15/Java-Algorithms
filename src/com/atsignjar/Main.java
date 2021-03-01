2655b1b8006be924750201362f2daaaa76ea53e8
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-01 16:10:58 -0500

    hash map phone book

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -3,10 +3,12 @@ package com.atsignjar;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
 //      IntroToConditionalStatements.Conditionals();
@@ -17,8 +19,9 @@ public class Main {
 //      Loops.Multiples();
 //      StringReview.Review();
 
-        Arrays.Array();
+//        Arrays.Array();
 
+        DictionariesAndMaps.HashMaps();
     }
 }
 
