5904f10954a7fb299e16f996fcaaf030092139ec
Jeremy Parker <jeremyeparker@yahoo.com>
2021-03-04 19:59:15 -0500

    loop through binary numbers and stop at first 0 then count all the 1s

--- src/com/atsignjar/Main.java
+++ src/com/atsignjar/Main.java
@@ -23,7 +23,24 @@ public class Main {
 
 //        DictionariesAndMaps.HashMaps();
 
-        Recursion.Recursion();
+//        Recursion.Recursion();
+        System.out.println("Enter a integer to find it's binary equivalent ");
+        int n = scanner.nextInt();
+        System.out.println("Binary of " + n + " is " + Integer.toString(n,2) );
+        String binary = Integer.toString(n,2);
+        int consecutiveOnes = 0;
+        for (int i = 0; i < binary.length(); i++){
+            System.out.println(binary.charAt(i));
+            if(binary.charAt(i) == 1){
+                consecutiveOnes ++;
+            }else{
+//                return consecutiveOnes
+                System.out.println(consecutiveOnes);
+
+            }
+        }
+
+
     }
 }
 
