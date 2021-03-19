4ae3730ef4004466faf82666d4a4619ccb785d96
Jeremy <jeremyeparker@yahoo.com>
2021-03-19 15:14:30 -0400

    create Calculator class, power method that thrown exceptions for negative numbers

--- src/com/atsignjar/Exceptions.java
+++ src/com/atsignjar/Exceptions.java
@@ -15,4 +15,43 @@ public class Exceptions {
             System.out.println("Bad String");
         }
     }
+
 }
+
+class Calculator{
+
+    int power(int n , int p) throws Exception{
+
+        if (n < 0 || p < 0){
+            throw new Exception("n and p should be non-negative");
+        }
+
+        int calc = (int) Math.pow(n,p);
+
+        return calc;
+    }
+}
+
+class Solution{
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            }
+            catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+}
+
