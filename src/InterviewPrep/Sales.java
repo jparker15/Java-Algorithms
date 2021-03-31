b37a45772de6d6b23cede567dc08685df6b712be
Jeremy <jeremyeparker@yahoo.com>
2021-03-31 19:55:28 -0400

    socks pair

--- src/InterviewPrep/Sales.java
+++ src/InterviewPrep/Sales.java
@@ -12,9 +12,29 @@ public class Sales {
 
 
         // Complete the sockMerchant function below.
-        static void sockMerchant(int n, int[] ar) {
+        static int sockMerchant(int n, int[] ar) {
+            int pairs = 0;
+            int count = 0;
 
+            HashMap<Integer, Integer> socks = new HashMap<Integer, Integer>();
 
+            // int[10] arr = [1,2,3,4,5,6,6,6,7,8]
+
+            for (int i = 0; i < n ; i++) {
+                if(socks.containsKey(ar[i])){
+                    socks.put(ar[i],socks.get(ar[i]) + 1);
+                }else{
+                    socks.put(ar[i], 1);
+                }
+            }
+            //in Java 3/2 = 1;
+            // 7/3 = 2;
+            for(int i: socks.values()){
+                pairs += i / 2;
+            }
+
+            System.out.println(socks);
+            return pairs;
         }
 
         private static final Scanner scanner = new Scanner(System.in);
