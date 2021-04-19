3bf9ff1fce7a86a14beb695187dbc02d035598cb
Jeremy <jeremyeparker@yahoo.com>
2021-04-18 23:52:56 -0400

    find largest num of n amt of ints

---
+++ src/CodeSignal/LargestNum.java
@@ -0,0 +1,12 @@
+package CodeSignal;
+
+public class LargestNum {
+    int largestNumber(int n) {
+        int largest = 10;
+        for(int i = 1; i < n; i++){
+            largest *= 10;
+        }
+        return largest -1;
+    }
+
+}
