74dc0eae47b27671bed18f5851e65dc968f24e33
Jeremy <jeremyeparker@yahoo.com>
2021-04-19 23:58:20 -0400

    git commit -m

---
+++ src/CodeSignal/checkPalindrome.java
@@ -0,0 +1,13 @@
+package CodeSignal;
+
+public class checkPalindrome {
+    boolean checkPalindrome(String inputString) {
+        String rev = "";
+
+        for(int i = inputString.length() - 1; i >= 0; i--){
+            rev += inputString.charAt(i);
+        }
+        return rev.equals(inputString);
+    }
+
+}
