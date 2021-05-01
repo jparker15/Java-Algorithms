6d3a84174015040de9e58b7e0f453bc726995ffd
Jeremy <jeremyeparker@yahoo.com>
2021-04-30 23:24:10 -0400

    abosolute diffe

--- src/InterviewPrep/containsCloseNums.java
+++ src/InterviewPrep/containsCloseNums.java
@@ -3,6 +3,14 @@ package InterviewPrep;
 public class containsCloseNums {
     boolean containsCloseNums(int[] nums, int k) {
 
+        for(int i = 0; i < nums.length; i++){
+            // int j = i + 1;
+            System.out.println("i" + nums[i]);
+            // System.out.println("j" + nums[j]);
+
+        }
+        return true;
+
         return true;
     }
 
