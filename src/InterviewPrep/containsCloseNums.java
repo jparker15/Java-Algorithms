f8fc7d8f18f7279df9e500ea6fbdc02f63baa558
Jeremy <jeremyeparker@yahoo.com>
2021-05-02 23:39:38 -0400

    10/12 test cases

--- src/InterviewPrep/containsCloseNums.java
+++ src/InterviewPrep/containsCloseNums.java
@@ -3,12 +3,19 @@ package InterviewPrep;
 public class containsCloseNums {
     boolean containsCloseNums(int[] nums, int k) {
 
+        if(nums.length <= 1){
+            return false;
+        }
+
         for(int i = 0; i < nums.length; i++){
 
             for(int j = i + 1; j < nums.length; j++){
+                //if there's dup nums
                 if(nums[i] == nums[j]){
                     int diff = Math.abs(i - j);
+                    System.out.println("diff:" + diff);
                     if(diff > k ){
+                        System.out.println(i+ " " + j);
                         return false;
                     }
 
