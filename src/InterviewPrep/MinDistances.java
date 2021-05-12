8ff4444ba20804686aad96a48b5ac98bc7199c9e
Jeremy Parker <jeremyeparker@yahoo.com>
2021-05-11 21:12:53 -0400

    hakcos

--- src/InterviewPrep/MinDistances.java
+++ src/InterviewPrep/MinDistances.java
@@ -11,27 +11,17 @@ public class MinDistances {
      */
 
     public static int minimumDistances(List<Integer> a) {
-        // Write your code here
-
-        List<Integer> b = new ArrayList<Integer>();
-
-
+        // Write your code her
 
         for(int i = 0; i < a.size(); i++){
             for(int j = i + 1; j < a.size(); j++){
                 if(a.get(i) == a.get(j)){
 
-                    b.add(Math.abs(i-j));
-
                 }
             }
         }
         // System.out.println(b);
-        if(b.size() > 0){
-            Collections.sort(b);
-            // System.out.println(b.get(0));
-            return b.get(0);
-        }
+
         return -1;
     }
 
