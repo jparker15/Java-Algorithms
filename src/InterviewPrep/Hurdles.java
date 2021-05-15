1477686425e046b9772d3d1db2632891391a5cea
Jeremy Parker <jeremyeparker@yahoo.com>
2021-05-14 23:27:48 -0400

    hurdles

--- src/InterviewPrep/Hurdles.java
+++ src/InterviewPrep/Hurdles.java
@@ -1,16 +1,5 @@
 package InterviewPrep;
 
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 public class Hurdles {
 //
@@ -47,29 +36,5 @@ public class Hurdles {
 //
 //    }
 //
-//    public class Solution {
-//        public static void main(String[] args) throws IOException {
-//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-//
-//            String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-//
-//            int n = Integer.parseInt(firstMultipleInput[0]);
-//
-//            int k = Integer.parseInt(firstMultipleInput[1]);
-//
-//            List<Integer> height = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-//                    .map(Integer::parseInt)
-//                    .collect(toList());
-//
-//            int result = Result.hurdleRace(k, height);
-//
-//            bufferedWriter.write(String.valueOf(result));
-//            bufferedWriter.newLine();
-//
-//            bufferedReader.close();
-//            bufferedWriter.close();
-//        }
-//    }
 
 }
