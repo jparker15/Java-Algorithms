871a0066e4757c94a2c8b33b130a8ef0dbd10272
Jeremy <jeremyeparker@yahoo.com>
2021-06-03 23:40:12 -0700

    i gotta bone to pick with this algorithm

---
+++ src/InterviewPrep/Bonetrousle.java
@@ -0,0 +1,24 @@
+package InterviewPrep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bonetrousle {
+
+    /*
+     * Complete the 'bonetrousle' function below.
+     *
+     * The function is expected to return a LONG_INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. LONG_INTEGER n
+     *  2. LONG_INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static List<Long> bonetrousle(long n, long k, int b) {
+        // Write your code here
+        ArrayList<Long> list = new ArrayList<Long>();
+        list.add(-1L);
+        return list;
+    }
+}
