00fe223a42468589d309090e37350f958e60c8a2
Jeremy <jeremyeparker@yahoo.com>
2021-06-04 20:41:01 -0400

    add psuedocode rules

--- src/InterviewPrep/Bonetrousle.java
+++ src/InterviewPrep/Bonetrousle.java
@@ -13,6 +13,19 @@ public class Bonetrousle {
      *  1. LONG_INTEGER n
      *  2. LONG_INTEGER k
      *  3. INTEGER b
+     *
+     * // Make a list/collection with default values (1 - b)
+    // Add the numbers in collection
+    // If its greater than N return -1
+    // If its equal to N return collection
+    // If its less than  N
+    // Calculate difference from N - sum of array
+    // Add the difference to last element of collection
+    // Check if new number is less than or equal to K
+    // return new collection
+    // If not greater than K set element to K and K - 1
+    // Repeat with next element
+    // If I run out of elements return -1
      */
 
     public static List<Long> bonetrousle(long n, long k, int b) {
