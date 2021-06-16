c0f8de9d72028ab145cf9eb52a03d3719a81f739
Jeremy <jeremyeparker@yahoo.com>
2021-06-15 21:02:37 -0400

    solve balance partheses

--- src/New/Result.java
+++ src/New/Result.java
@@ -1,8 +1,36 @@
 package New;
 
+import java.util.Stack;
+
 public class Result {
 
+    public static void main(String[] args) {
+       System.out.println(getMin("(((((("));
+    }
+
     public static int getMin(String s){
-        return 0;
+        //Init stack
+        //iterate each char in input string
+        //only two possible chars
+        //if '(' or stack is empty add to stack
+        // else
+            // if peek == '(' pop stack
+            // else add to stack
+        //return stack.size()
+
+        Stack<Character> stack = new Stack<>();
+        for(char c: s.toCharArray()){
+            if(c == '(' || stack.empty() == true) {
+                stack.add(c);
+            }else{
+                if(stack.peek() == '('){
+                    stack.pop();
+                }else{
+                    stack.add(c);
+                }
+            }
+        }
+
+        return stack.size();
     }
 }
