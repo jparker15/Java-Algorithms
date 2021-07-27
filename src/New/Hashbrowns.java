6c298d7967bcb9d5aabfe76766c17d8896dee834
Jeremy <jeremyeparker@yahoo.com>
2021-07-26 21:10:44 -0400

    1/3 testcases

--- src/New/Hashbrowns.java
+++ src/New/Hashbrowns.java
@@ -1,53 +1,55 @@
 package New;
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+
+import java.util.HashMap;
 
 public class Hashbrowns {
 
-        // Complete the checkMagazine function below.
-        static void checkMagazine(String[] magazine, String[] note) {
 
+    public static void checkMag (String[] magazine, String[] note){
 
+        HashMap<String,Integer> magazineMap = new HashMap<String,Integer>();
+        HashMap<String,Integer> noteMap = new HashMap<String,Integer>();
+
+        for (int i = 0; i < magazine.length ;i++){
+            if(magazineMap.get(magazine[i]) != null) {
+                magazineMap.put(magazine[i],magazineMap.get(magazine[i]) + 1);
+            } magazineMap.put(magazine[i],1);
         }
 
-        private static final Scanner scanner = new Scanner(System.in);
-
-        public static void main(String[] args) {
-            String[] mn = scanner.nextLine().split(" ");
-
-            int m = Integer.parseInt(mn[0]);
-
-            int n = Integer.parseInt(mn[1]);
-
-            String[] magazine = new String[m];
-
-            String[] magazineItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int i = 0; i < m; i++) {
-                String magazineItem = magazineItems[i];
-                magazine[i] = magazineItem;
-            }
-
-            String[] note = new String[n];
-
-            String[] noteItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int i = 0; i < n; i++) {
-                String noteItem = noteItems[i];
-                note[i] = noteItem;
-            }
-
-            checkMagazine(magazine, note);
-
-            scanner.close();
+        for(String s: note){
+            if(noteMap.get(s) != null) {
+                noteMap.put(s,noteMap.get(s) + 1);
+            } noteMap.put(s,1);
         }
 
+        for(int i = 0; i < magazine.length ;i++){
 
+            if(noteMap.get(magazine[i]) != magazineMap.get( magazine[i])){
+                System.out.println("blad");
+            }
+        }
+
+        noteMap.equals(magazineMap);
+        for(String key: noteMap.keySet()){
+            System.out.println(key);
+            System.out.println(noteMap.get(key));
+        }
+
+        for(String key: magazineMap.keySet()){
+
+            for(String k: noteMap.keySet()){
+
+                if(key.equals(k) && magazineMap.get(k) == noteMap.get(k)){
+                    yn = true;
+                }
+            }
+        }
+
+        if(yn){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+
+    }
 }
