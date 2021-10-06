97f47b97d84a7f1dfea16d44a73f872bf07c14a6
Jeremy <jeremyeparker@yahoo.com>
2021-10-06 18:49:55 -0400

    ransome note with up and down hashmap

---
+++ src/Algo/RansomNote.java
@@ -0,0 +1,38 @@
+package Algo;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class RansomNote {
+    /*
+     * Complete the 'checkMagazine' function below.
+     *
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY magazine
+     *  2. STRING_ARRAY note
+     */
+
+    public static void checkMagazine(List<String> magazine, List<String> note) {
+        // Write your code here
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+
+        for(String s: magazine){
+
+            hm.merge(s, 1, Integer::sum);
+
+        }
+
+        for(String s: note){
+            if(!hm.containsKey(s) || hm.get(s) <= 0){
+                System.out.println("No");
+                return;
+
+            }else{
+                hm.put(s, hm.get(s) - 1);
+            }
+        }
+
+        System.out.println("Yes");
+    }
+
+}
